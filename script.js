559ab2f3f40ee43c0a715916c4f935e4cb05c79d
Joyce Li <lijoyce072461@gmail.com>
2024-01-17 00:57:06 -0500

    added everything

---
+++ script.js
@@ -0,0 +1,23 @@
+
+function onButtonClick() {
+    number = Math.floor(Math.random() * 5);
+    if (number == oldNum) {
+        number += 1 % 5;
+    }
+    switch (number) {
+        case 0:
+            document.getElementById("funFact").innerHTML = 'Joyce has a crippling addiction to mobile games.';
+            break;
+        case 1:
+            document.getElementById("funFact").innerHTML = "Joyce's favorite cake is cheesecake.";
+            break;
+        case 2:
+            document.getElementById("funFact").innerHTML = "Joyce has a two foot tall stuffed Kumamon that she cannot sleep without.";
+            break;
+        case 3:
+            document.getElementById("funFact").innerHTML = "Joyce owns a puffy green coat that she wears anytime it's below 60 degrees.";
+            break;
+        case 4:
+            document.getElementById("funFact").innerHTML = "Joyce has lactose intolerace but pretends it doesn't exist.";
+    }
+}
